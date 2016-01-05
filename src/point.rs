6fd8b29be1c5df8e6265c5bbbbbe735c40bd2bd9
Yannick Heinrich <yannick.heinrich@gmail.com>
2016-01-05 23:46:41 +0100

    Add copy, clone with derive

--- src/point.rs
+++ src/point.rs
@@ -1,36 +1,72 @@
-enum Unit {
+extern crate std;
+
+#[derive(Clone, Copy)]
+enum AngleUnit {
    Radian,
    Degree,
-   Meter,
    Grad,
 }
 
-fn factor(from:Unit, to:Unit) -> f32 {
+fn factor(from:AngleUnit, to:AngleUnit) -> f32 {
     match (from, to) {
         // Logical conversion
-        (Unit::Degree, Unit::Degree) => 1.0,
-        (Unit::Radian, Unit::Radian) => 1.0,
-        (Unit::Meter, Unit::Meter) => 1.0,
-        (Unit::Grad, Unit::Grad) => 1.0,
+        (AngleUnit::Degree, AngleUnit::Degree) => 1.0,
+        (AngleUnit::Radian, AngleUnit::Radian) => 1.0,
+        (AngleUnit::Grad, AngleUnit::Grad) => 1.0,
 
-        //-> Radian
-        (Unit::Degree, Unit::Radian) => 
+        //Degree <-> Radian
+        (AngleUnit::Degree, AngleUnit::Radian) => std::f32::consts::PI / 180.0,
+        (AngleUnit::Radian, AngleUnit::Degree) => 180.0 / std::f32::consts::PI ,
+
+        //Grad <-> Radian
+        (AngleUnit::Grad, AngleUnit::Radian) => std::f32::consts::PI / 200.0,
+        (AngleUnit::Radian, AngleUnit::Grad) => 200.0 / std::f32::consts::PI,
+
+        //Degree <-> Grad
+        (AngleUnit::Degree, AngleUnit::Grad) => 200.0/180.0,
+        (AngleUnit::Grad, AngleUnit::Degree) => 180.0/200.0,
+
     }
 }
+
 struct Point {
     x: f32,
     y: f32,
     z: f32,
-    unit: Unit,
+    unit: AngleUnit,
 }
 
 impl Point {
-    fn new(x: f32, y: f32, z: f32, unit: Unit) -> Point {
+    pub fn new(x: f32, y: f32, z: f32, unit: AngleUnit) -> Point {
         Point { x: x, y: y, z: z, unit: unit}
     }
+
     fn scale(&mut self, factor:f32) {
         self.x = self.x * factor;
         self.y = self.y * factor;
         self.z = self.z * factor;
     }
+
+    fn convert(&mut self, unit:AngleUnit) {
+        let point_unit = self.unit;
+        self.scale(factor(point_unit, unit));
+    }
+
+}
+
+#[test]
+fn test_new(){
+    let point = Point::new(55.0,1.0,0.0,AngleUnit::Degree);
+    assert_eq!(point.x, 55.0);
+    assert_eq!(point.y, 1.0);
+    assert_eq!(point.z, 0.0);
+}
+
+#[test]
+fn test_scale(){
+    let mut point = Point::new(55.0,1.0,0.0,AngleUnit::Degree);
+    point.scale(2.0);
+    assert_eq!(point.x, 110.0);
+    assert_eq!(point.y, 2.0);
+    assert_eq!(point.z, 0.0);
 }
